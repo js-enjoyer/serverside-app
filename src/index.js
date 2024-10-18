d0c8b16470342d95c1a04710ec7e11feb8c86b2c
js-enjoyer <vasilen.tihin@gmail.com>
2024-10-18 04:46:57 +0300

    Adding static middleware
    Setting up resources extension names, adding static middleware for the css request, testing home page.

--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ app.engine('hbs', handlebars.engine( { extname: 'hbs' } ))
 app.set('view engine', 'hbs')
 app.set('views', './src/views')
 
+app.use('/static', express.static('./src/static'))
+app.use('/static/img', express.static('static'))
+
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('home')
+})
+app.get('/home', (req, res) => {
+    res.render('home')
 })
 
 app.listen(5000, () => console.log('Server listening on port http//localhost:5000...'))
