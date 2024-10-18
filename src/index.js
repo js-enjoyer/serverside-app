6ef45f788f49f58005e638b85a44f57e0a1ecbf0
js-enjoyer <vasilen.tihin@gmail.com>
2024-10-18 18:32:29 +0300

    Adding modular router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 
+import routes from './routes.js'
+
 const app = express()
 
 app.engine('hbs', handlebars.engine( { extname: 'hbs' } ))
@@ -10,11 +12,6 @@ app.set('views', './src/views')
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-app.get('/home', (req, res) => {
-    res.render('home')
-})
+app.use(routes)
 
 app.listen(5000, () => console.log('Server listening on port http//localhost:5000...'))
