979f2c203b2904afe0aa6dc68c55b15ebf93f770
js-enjoyer <vasilen.tihin@gmail.com>
2024-10-18 04:22:22 +0300

    Setting up handlebars

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+
+const app = express()
+
+app.engine('hbs', handlebars.engine( { extname: 'hbs' } ))
+
+app.set('view engine', 'hbs')
+app.set('views', './src/views')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(5000, () => console.log('Server listening on port http//localhost:5000...'))
